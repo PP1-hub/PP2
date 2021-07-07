0a9d27b1c7a5de681593dd186232f7701b842626
Anar <78043093+PP1-hub@users.noreply.github.com>
2021-07-07 19:55:40 +0600

    Delete 3.py

--- 3.py
+++
@@ -1,8 +0,0 @@
-class Solution(object):
-    def numIdenticalPairs(self, nums):
-        n = 0
-        for count, value in enumerate(nums):
-            for x in nums[count+1:]:
-                if x == value:
-                    n+=1
-        return n  
